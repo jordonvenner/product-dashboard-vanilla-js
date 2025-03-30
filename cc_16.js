99511358204c97929dbcd82b474abe32dbdd654f
Jordon Venner <jordonvenner@usf.edu>
2025-03-29 23:04:30 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -9,3 +9,14 @@ function fetchProductsThen() {
       })
       .catch((error) => console.log(error));
   }
+
+  //task 3
+  async function fetchProductsAsync() {
+    try {
+      const response = await fetch('https://www.course-api.com/javascript-store-products');
+      const products = await response.json();
+      displayProducts(products);
+    } catch (error) {
+      handleError(error);
+    }
+  }
